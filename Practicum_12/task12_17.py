1f40741dd6d0f25c13149854366dc56486d0577a
llensaa <Lisazaza186L@gmail.com>
2025-09-16 09:55:34 +0700

    Update task12_17.py

--- Practicum_12/task12_17.py
+++ Practicum_12/task12_17.py
@@ -1,8 +1,1 @@
-from re import *
 
-s = input()
-
-while search(r'\([^()]+\)', s):
-    s = sub(r'\(([^()]+)\)', lambda m: str(sum(map(int, m.group(1).split('+')))), s)
-
-print(sum(map(int, s.split('+'))))
