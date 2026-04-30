ef79b1ec29706efe0479be6651781988a35d17f5
llensaa <Lisazaza186L@gmail.com>
2026-04-30 07:00:40 +0700

    Update task19_3.py

--- Practicum_19/task19_3.py
+++ Practicum_19/task19_3.py
@@ -1,14 +1,25 @@
 class NotSleeping:
+    """Класс, представляющий засыпающего человека, который считает овец."""
     
     def __init__(self, name, count_sheeps=0):
+        """
+        Инициализация человека.
+        
+        Параметры:
+        name - имя человека
+        count_sheeps - начальное количество овец (по умолчанию 0)
+        """
         self.name = name
         self.count_sheeps = count_sheeps
     
     def add_sheep(self):
+        """Добавляет одну овцу к счету."""
         self.count_sheeps += 1
     
     def lost(self):
+        """Сбрасывает счет овец (человек сбился со счета)."""
         self.count_sheeps = 0
     
     def get_count_sheeps(self):
+        """Возвращает текущее количество овец."""
         return self.count_sheeps
