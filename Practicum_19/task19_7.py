60262d21af088a71721d6d1a5d6404c0d1ad90d0
llensaa <Lisazaza186L@gmail.com>
2026-04-30 07:01:37 +0700

    Update task19_7.py

--- Practicum_19/task19_7.py
+++ Practicum_19/task19_7.py
@@ -1,11 +1,14 @@
 class TrafficLight:
+    """Класс, представляющий светофор."""
     
     permissible_values = ['зеленый', 'желтый', 'красный', 'желтый']
     
     def __init__(self):
+        """Инициализация светофора. Начальный сигнал - зеленый."""
         self.current_signal = 'зеленый'
         self.current_index = 0
     
     def next_signal(self):
+        """Переключает светофор на следующий сигнал."""
         self.current_index = (self.current_index + 1) % len(self.permissible_values)
         self.current_signal = self.permissible_values[self.current_index]
