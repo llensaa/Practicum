b503b464cd19b51e867b71db67ecd54cd8486adc
llensaa <Lisazaza186L@gmail.com>
2026-04-30 07:01:53 +0700

    Update task19_8.py

--- Practicum_19/task19_8.py
+++ Practicum_19/task19_8.py
@@ -1,5 +1,7 @@
 class MorseMsg:
+    """Класс, представляющий сообщение на азбуке Морзе."""
     
+    # Словарь для декодирования (латиница)
     morse_to_eng = {
         '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
         '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
@@ -9,6 +11,7 @@ class MorseMsg:
         '--..': 'Z'
     }
     
+    # Словарь для декодирования (кириллица)
     morse_to_ru = {
         '.-': 'А', '-...': 'Б', '.--': 'В', '--.': 'Г', '-..': 'Д',
         '.': 'Е', '...-': 'Ж', '--..': 'З', '..': 'И', '.---': 'Й',
@@ -19,14 +22,22 @@ class MorseMsg:
         '..--': 'Ю', '.-.-.': 'Я'
     }
     
+    # Гласные буквы
     vowels_eng = set('AEIOU')
     vowels_ru = set('АЕЁИОУЫЭЮЯ')
     
     def __init__(self, encoded_msg):
+        """
+        Инициализация сообщения.
+        
+        Параметры:
+        encoded_msg - строка с закодированным сообщением
+        """
         self.encoded_msg = encoded_msg
         self.morse_words = encoded_msg.split(' ')
     
     def eng_decode(self):
+        """Декодирует сообщение на латинице."""
         result = ''
         for morse in self.morse_words:
             if morse in self.morse_to_eng:
@@ -34,6 +45,7 @@ class MorseMsg:
         return result
     
     def ru_decode(self):
+        """Декодирует сообщение на кириллице."""
         result = ''
         for morse in self.morse_words:
             if morse in self.morse_to_ru:
@@ -41,6 +53,7 @@ class MorseMsg:
         return result
     
     def get_vowels(self, lang):
+        """Возвращает список гласных букв в порядке следования."""
         if lang == 'eng':
             decoded = self.eng_decode()
             return [ch for ch in decoded if ch in self.vowels_eng]
@@ -49,6 +62,7 @@ class MorseMsg:
             return [ch for ch in decoded if ch in self.vowels_ru]
     
     def get_consonants(self, lang):
+        """Возвращает список согласных букв в порядке следования."""
         if lang == 'eng':
             decoded = self.eng_decode()
             return [ch for ch in decoded if ch not in self.vowels_eng]
@@ -57,4 +71,5 @@ class MorseMsg:
             return [ch for ch in decoded if ch not in self.vowels_ru]
     
     def __str__(self):
+        """Строковое представление сообщения."""
         return f"MorseMsg('{self.encoded_msg}')"
