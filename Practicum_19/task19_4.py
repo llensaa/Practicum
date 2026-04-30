2dfa92df1fa6facf0ac2364f786900bc25020685
llensaa <Lisazaza186L@gmail.com>
2026-04-30 07:00:56 +0700

    Update task19_4.py

--- Practicum_19/task19_4.py
+++ Practicum_19/task19_4.py
@@ -1,6 +1,18 @@
 class User:
+    """Класс, представляющий пользователя сайта."""
     
     def __init__(self, id, nick_name, first_name, last_name='', middle_name='', gender=''):
+        """
+        Инициализация пользователя.
+        
+        Параметры:
+        id - уникальный номер пользователя (обязательный)
+        nick_name - псевдоним пользователя (обязательный)
+        first_name - имя пользователя (обязательный)
+        last_name - фамилия (необязательный)
+        middle_name - отчество (необязательный)
+        gender - пол (необязательный)
+        """
         self.id = id
         self.nick_name = nick_name
         self.first_name = first_name
@@ -9,6 +21,10 @@ class User:
         self.gender = gender
     
     def update(self, id=None, nick_name=None, first_name=None, last_name=None, middle_name=None, gender=None):
+        """
+        Обновляет атрибуты пользователя.
+        Если параметр не передан, атрибут не изменяется.
+        """
         if id is not None:
             self.id = id
         if nick_name is not None:
@@ -23,4 +39,5 @@ class User:
             self.gender = gender
     
     def __str__(self):
+        """Строковое представление пользователя."""
         return f"User(id={self.id}, nick_name='{self.nick_name}', first_name='{self.first_name}')"
